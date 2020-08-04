5bfd82de96bfb30e6337e86c12d6550d4d072a54
xxxt <a867965056@gmail.com>
2020-08-04 15:27:38 +0800

    add comment to admin

--- comment/admin.py
+++ comment/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
 # Register your models here.
+
+from comment import models
+# from blog.forms import UserForm
+# Register your models here.
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('article', 'user', 'body', 'created')
+
+
+admin.site.register(models.Comment)
+admin.site.register(models.Comment, CommentAdmin)
